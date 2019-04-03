f4de64b7fb5be09f3f2893e9b15fff98495e024d
Shixiaowei02 <shixiaowei02@baidu.com>
2019-04-03 22:13:28 +0800

    remove log

--- paddle/fluid/inference/anakin/engine.cc
+++ paddle/fluid/inference/anakin/engine.cc
@@ -87,16 +87,6 @@ void AnakinEngine<TargetT, PrecisionType, RunType>::Execute(
     int max_shape_sum =
         std::accumulate(max_input_shape.begin(), max_input_shape.end(), 1,
                         std::multiplies<int>());
-    for (auto max_val : max_input_shape) {
-      LOG(INFO) << "[MAX SHAPE]: " << max_val;
-    }
-
-    for (auto act_val : framework::vectorize2int(tensor->dims())) {
-      LOG(INFO) << "[ACT SHAPE]: " << act_val;
-    }
-    for (auto in : program_inputs_) {
-      LOG(INFO) << "[PROGRAM INPUTS]: " << in;
-    }
     if (tensor->numel() > max_shape_sum) {
       PADDLE_ENFORCE(std::find(program_inputs_.begin(), program_inputs_.end(),
                                input.first) == program_inputs_.end(),
