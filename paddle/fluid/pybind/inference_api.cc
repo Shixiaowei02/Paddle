880763c068324751e458473d967f39e29f04877a
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2020-03-10 05:22:48 +0000

    change the cmake of lite engine, test=develop

--- paddle/fluid/pybind/inference_api.cc
+++ paddle/fluid/pybind/inference_api.cc
@@ -413,6 +413,10 @@ void BindAnalysisConfig(py::module *m) {
            py::arg("min_subgraph_size") = 3,
            py::arg("precision_mode") = AnalysisConfig::Precision::kFloat32,
            py::arg("use_static") = false, py::arg("use_calib_mode") = true)
+      .def("enable_lite_engine", &AnalysisConfig::EnableLiteEngine,
+           py::arg("precision_mode") = AnalysisConfig::Precision::kFloat32,
+           py::arg("passes_filter") = std::vector<std::string>(),
+           py::arg("ops_filter") = std::vector<std::string>())
       .def("tensorrt_engine_enabled", &AnalysisConfig::tensorrt_engine_enabled)
       .def("switch_ir_debug", &AnalysisConfig::SwitchIrDebug,
            py::arg("x") = true)
