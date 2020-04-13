12f18f5b0887ab8f093c57fd38547e3981eb8ba2
Shixiaowei02 <shixiaowei02@baidu.com>
2020-04-13 16:35:26 +0800

    tmp commit

---
+++ paddle/fluid/memory/allocation/stream_allocator.cc
@@ -0,0 +1,49 @@
+// Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "paddle/fluid/memory/allocation/stream_allocator.h"
+
+namespace paddle {
+namespace memory {
+namespace allocation {
+
+class StreamAllocator : public Allocation {
+ public:
+  StreamAllocaton(void* ptr, size_t size, platform::Place place)
+      : Allocation(ptr, size, place) {}
+
+  void SetStreamAllocator(std::shared_ptr<Allocator> allocator) {
+    allocator_ = allocator;
+  }
+
+ private:
+  std::shared_ptr<Allocator> allocator_;
+};
+
+Allocation* ThreadLocalAllocator::AllocateImpl(size_t size) {
+  void* ptr = buddy_allocator_->Alloc(size);
+  auto* tl_allocation = new StreamAllocaton(ptr, size, place_);
+  tl_allocation->SetStreamAllocator(shared_from_this());
+  return tl_allocation;
+}
+
+void ThreadLocalAllocator::FreeImpl(Allocation* allocation) {
+  auto* tl_allocation = static_cast<StreamAllocaton*>(allocation);
+  buddy_allocator_->Free(tl_allocation->ptr());
+  delete tl_allocation;
+}
+
+}  // namespace allocation
+}  // namespace memory
+}  // namespace paddle
